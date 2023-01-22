12d024301c3347975ba34883828bae5ee2feb648
alice <zhupanova2017sv@mail.ru>
2023-01-22 22:10:56 +0300

    Second num

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа. 457
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num / 100 != 0)
+{
+    int result1 = num % 100;
+    int result2 = result1 / 10;
+    System.Console.WriteLine($"{result2}");
+}
+
+else
+{
+    Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число");
+}
