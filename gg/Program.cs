1e148c7dbed19ef6c45dfa0901a6911873f2d907
alice <zhupanova2017sv@mail.ru>
2023-01-23 21:04:29 +0300

    gitignore

---
+++ gg/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+while (num / 1000 != 0)
+{
+    System.Console.WriteLine($"{num} - Третьей цифры нет!");
+}
+num = num / 100;
+Console.WriteLine($"{num}");
