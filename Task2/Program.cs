de1815736e46c002e66ab60327bcda796c1f4fab
alice <zhupanova2017sv@mail.ru>
2023-01-23 19:16:04 +0300

    Last num

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num / 100 != 0)
+{
+    int numLast = num % 10;
+    System.Console.WriteLine($"{num} -> {numLast}");
+}
+else
+{
+    System.Console.WriteLine($"{num} - Третьей цифры нет!");
+}
